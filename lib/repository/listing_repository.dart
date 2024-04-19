5e96377ba2601f61781e9e46c5573f897acf1c35
KyleFuehri-Prodapt <kyle.fuhri@yahoo.com>
2024-04-19 20:39:18 +0100

    Update listing_repository.dart

--- lib/repository/listing_repository.dart
+++ lib/repository/listing_repository.dart
@@ -5,14 +5,20 @@ import 'package:flutter/foundation.dart';
 import 'package:funda_demo/models/listing_detail_model.dart';
 import 'package:funda_demo/network/network_provider.dart';
 
+/// Repository for fetching listing details from the network.
 class ListingRepository {
   final NetworkProvider networkProvider;
   final String apiKey;
   ListingDetailModel? _cachedListing;
 
+  /// Constructs a [ListingRepository] with the given [networkProvider] and [apiKey].
   ListingRepository({required this.networkProvider, required this.apiKey});
 
-  /// Fetches a single listing by ID
+  /// Fetches a single listing by ID.
+  ///
+  /// If [forceRefresh] is `false` (default), the repository will return the cached listing if available.
+  /// If [forceRefresh] is `true`, or if no cached listing is available, the repository will fetch the listing details from the network.
+  /// Returns the [ListingDetailModel] if successful, otherwise returns `null`.
   Future<ListingDetailModel?> getListingById(String listingId, {bool forceRefresh = false}) async {
     if (_cachedListing != null && !forceRefresh) {
       return _cachedListing;
